19fa0fd8dc307b7a96b1280345eff69ffdab4d31
madmetal7 <ed.isoe93@gmail.com>
2026-04-07 08:55:27 +0300

    Isolate test config roots

---
+++ DeezSpoTag.Tests/SettingsConfigIsolationCollection.cs
@@ -0,0 +1,8 @@
+using Xunit;
+
+namespace DeezSpoTag.Tests;
+
+[CollectionDefinition("Settings Config Isolation", DisableParallelization = true)]
+public sealed class SettingsConfigIsolationCollection
+{
+}
